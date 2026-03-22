429228f4d5f0d3b0c08c4a1fdcd9043f1db522f2
Jay R. Newlin <jay@jaynewlin.com>
2026-03-22 17:19:47 -0400

    Quartz sync: Mar 22, 2026, 5:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "jaynewlin.me",
+    pageTitleSuffix: "Jay R. Newlin's Personal Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jaynewlin.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
